3d814f057465546f49f1cc58fefb2ae557b0df7b
vicrwei <61772244+vicrwei@users.noreply.github.com>
2020-08-18 07:52:33 +0200

    Verbesserungen am Programmcode

--- Widerstände/Program.cs
+++ Widerstände/Program.cs
@@ -23,70 +23,99 @@ namespace Widerstände
             int abfrage = 0;
             string weiterrechnen = "";
 
-            do
-            {
-                //Benutzereingabe der Widerstände
-                Console.WriteLine("Bitte geben Sie den Wiederstand R1 ein!");
-                try
-                {
-                    R1 = Convert.ToInt32(Console.ReadLine());
-                }
-
-                catch
-                {
-                    Console.WriteLine("Bitte geben Sie nur eine Zahl zwischen 0 und 2 147 483 647 ein!");
-                }
-
-                Console.WriteLine();
-
-                Console.WriteLine("Bitte geben Sie den Wiederstand R2 ein!");
-
-                try
-                {
-                    R2 = Convert.ToInt32(Console.ReadLine());
-                }
-                catch
-                {
-                    Console.WriteLine("Bitte geben Sie nur eine Zahl zwischen 0 und 2 147 483 647 ein!");
-                }
-                Console.WriteLine();
-            }
-            while ((R1 = 0)(R2 = 0));
-
-            do
-            {
-                Console.WriteLine("Handelt es sich um eine Reihenschaltung (1) oder eine Parallelschaltung (2)?");
-
-                try
-                {
-                    abfrage = Int32.Parse(Console.ReadLine());
-                }
-                catch
-                {
-                    Console.WriteLine("Bitte geben Sie nur (1) oder (2), ohne Klammer, ein");
-                }
-            }
-            while ((RR = 0) (RP = 0));
 
             do
             { 
+                do
+                {
+                    //Benutzereingabe der Widerstände
+                    Console.WriteLine("Bitte geben Sie den Wiederstand R1 ein!");
+                    try
+                    {
+                        R1 = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    catch
+                    {
+                        Console.WriteLine("Bitte geben Sie nur eine Zahl zwischen 0 und 2 147 483 647 ein!");
+                    }
+
+                    Console.WriteLine();
+                }
+                while (R1 == 0);
+
+                do 
+                {
+
+                    Console.WriteLine("Bitte geben Sie den Wiederstand R2 ein!");
+
+                    try
+                    {
+                        R2 = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Bitte geben Sie nur eine Zahl zwischen 0 und 2 147 483 647 ein!");
+                    }
+                    Console.WriteLine();
+                }
+                while (R2 == 0);
+
+                do
+                {
+                    Console.WriteLine("Handelt es sich um eine Reihenschaltung (1) oder eine Parallelschaltung (2)?");
+
+                    try
+                    {
+                        abfrage = Int32.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Bitte geben Sie nur (1) oder (2), ohne Klammer, ein");
+                    }
+                }
+                while (abfrage == 0);
+
+            
                 switch (abfrage)
                 {
                     //Reihenschaltung
                     case 1:     
                         {
                             RR = R1 + R2;
+
+                            Console.WriteLine("\nDer Gesamtwiderstand der Reihenschaltung beträgt: {0} Ohm. Der Widerstand für R1 beträgt {1} Ohm und für R2 {2} Ohm.\n\nFolgende Berechnungsformel wurde genutzt: RR = R1 + R2 => {0} = {1} + {2}", RR, R1, R2);
+
+                                                     Console.WriteLine("\n\nWollen Sie einen weiteren Gesamtwiderstand ausrechnen?");
+                            Console.WriteLine("Geben Sie bitte (j)a oder (n)ein in die Konsole!");
+
+                            weiterrechnen = Console.ReadLine();
+
                             break;
                         }
                     //Parallelschaltung
                     case 2:     
                         {
                             RP = (R1 * R2) / (R1 + R2);
+
+                            Console.WriteLine("\nDer Gesamtwiderstand der Parallelschaltung beträgt: {0} Ohm. Der Widerstand für R1 beträgt {1} Ohm und für R2 {2} Ohm.\n\nFür die Parallelschaltung wurde folgende Formel benutzt: RP = (R1 * R2)/(R1 + R2) => {0} = ({1} * {2})/({1) + {2})", RP, R1, R2);
+                                                        
+                            Console.WriteLine("\n\nWollen Sie einen weiteren Gesamtwiderstand ausrechnen?");
+                            Console.WriteLine("Geben Sie bitte (j)a oder (n)ein in die Konsole!");
+
+                            weiterrechnen = Console.ReadLine();
+
                             break;
                         }
                     default:
                         {
                             Console.WriteLine("Sie haben eine falsche Eingabe getätigt.");
+
+                            Console.WriteLine("Wollen Sie einen weiteren Gesamtwiderstand ausrechnen?");
+                            Console.WriteLine("Geben Sie bitte (j)a oder (n)ein in die Konsole!");
+
+                            weiterrechnen = Console.ReadLine();
+
                             break;
                         }
                 }
